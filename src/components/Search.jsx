b89443d49b33457ea709a819a0110268881021c0
Jacob Castanon <jacastanon01@gmail.com>
2023-06-25 14:35:26 -0500

    added grid functionality to search  bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,12 +7,12 @@ import styles from "./Search.module.scss"
 function Search() {
   const [query, setQuery] = useState('')
   return (
-    <StyledForm method="get" action={`/search`} onSubmit={() => setQuery('')}>
+    <Form method="get" action={`/search`} className={styles["search-form"]} onSubmit={() => setQuery('')}>
         <input className={styles["input-query"]} type="text" value={query} onChange={(e) => setQuery(e.target.value)} name="query" />
-        <div className={styles["search-icon-container"]}>
+        <div className={styles["search-icon"]}>
           <FaSearch />
         </div>
-    </StyledForm>
+    </Form>
   )
 }
 
