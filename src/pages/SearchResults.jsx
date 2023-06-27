f6cf37afaeeeabe0c51735d23f8890f87a145561
Jacob Castanon <jacastanon01@gmail.com>
2023-06-27 18:07:58 -0500

    used suspense and defer to optimize search UI

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,10 +1,46 @@
-import {useLoaderData} from "react-router-dom"
+import {useLoaderData, useSearchParams, defer, Await, json, useRouteError} from "react-router-dom"
 import styled from "styled-components"
 import Card from "../components/Card"
 import { motion } from "framer-motion"
+import { Suspense } from "react"
+import { SearchError } from "./SearchError"
+
+export async function getSearchResults({ request }) {
+    console.log(request)
+    let url = new URL(request.url);
+    let searchTerm = url.searchParams.get("query");
+    //console.log(searchTerm)
+    if (searchTerm) {
+        const fetchApi = fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_API_KEY}&query=${searchTerm}`).then(res => res.json())
+        //const result = fetchApi.json()
+        //console.log(result)
+        // if (result.totalResults === 0) {
+        //     throw json({
+        //         query: searchTerm,
+                
+        //     })
+        //console.log(fetchApi)
+        
+        return defer({searchResults: await fetchApi})
+    } else {
+        throw json({
+            status: 404,
+            message: `${searchTerm} not found`
+        })
+    }
+    //return null  
+    // throw json({
+    //     query: searchTerm
+    // })
+} 
+
+
 
 function SearchResults(){
     const data = useLoaderData()
+    //const [query, setQuery] = useSearchParams()
+    const error = useRouteError()
+    console.log(data.searchResults.results)
 
     return (
         <motion.section
@@ -14,9 +50,16 @@ function SearchResults(){
             exit={{opacity: 0}}
             transition={{duration: 0.5}}
           >
-            {data?.map((result) => (
-                <Card key={result.id} recipe={result} />               
-            ))}
+            <Suspense fallback={<p>Loading package location...</p>}>
+                <Await resolve={data.searchResults} errorElement={<p>{error?.data?.message}</p>}>
+                    {data.searchResults.results.length > 0 &&
+                        data.searchResults?.results.map((result) => (
+                            <Card key={result.id} recipe={result} />               
+                        )) 
+                    }
+                </Await>
+            </Suspense>
+            
         </motion.section>
     )
 
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,11 @@
 import RootLayout from "../layout/RootLayout"
 import Cuisine from "../pages/Cuisine"
 import Home from "../pages/Home"
-import { createBrowserRouter, Route, createRoutesFromElements } from 'react-router-dom'
+import { createBrowserRouter, Route, createRoutesFromElements, json } from 'react-router-dom'
 import SearchResults from "../pages/SearchResults"
 import Recipe from "../pages/Recipe"
 import { ErrorBoundary } from "../pages/ErrorBoundary"
+import { getSearchResults } from "../pages/SearchResults"
 
 
 const getCuisine = async ({ params }) => {
@@ -21,19 +22,7 @@ const getCuisine = async ({ params }) => {
     }
 }
 
-const getSearchResults = async ({ request }) => {
-    console.log(request)
-    let url = new URL(request.url);
-    let searchTerm = url.searchParams.get("query");
-    console.log(searchTerm)
-    if (searchTerm) {
-        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${import.meta.env.VITE_API_KEY}&query=${searchTerm}`)
-        const result = await data.json()
-        console.log(result)
-        return result.results
-    } else return null
 
-}
 
 const getRecipe = async ({ params }) => {
     const checkLocalStorage = localStorage.getItem(params.recipeId)
